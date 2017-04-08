65b968c608f4f6959922d94fb6b007f79e3d3bb2
wnagsiwen <2228910205@qq.com>
2017-04-08 13:47:43 +0800

    一休哥

---
+++ app/src/main/java/com/zykj/wswyixiu/activity/one.java
@@ -0,0 +1,16 @@
+package com.zykj.wswyixiu.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zykj on 2017/4/8.
+ */
+
+public class one extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
